793d0c40c3676dfb7da8885c76209528a2ee02f3
Hitesh Lalwani <79440952+xobe19@users.noreply.github.com>
2022-03-13 15:16:38 +0530

    added title for the website

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 import Navbar from '../components/navbar'
 import { Fragment } from 'react/cjs/react.production.min'
 import Script from 'next/script'
+import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
 function MyApp({ Component, pageProps }) {
 console.log(pageProps);
   return <Fragment>
+    <Head> <title> CVR Alumni </title> <Head>
     <div className='bg-white'>
     <Script src="https://kit.fontawesome.com/ce6e920b0f.js"></Script>
 <NextNProgress height={3} color="cyan" />
